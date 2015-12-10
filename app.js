4a6682c79bb9111c0288c4d0e22b7b7e170af5ea
Luke Vance <lukevance90@gmail.com>
2015-12-09 20:22:38 -0700

    assignment part 1 completed

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 'use strict';
+//
+$.ajax({
+    url: 'http://localhost:8000/api',
+    json: "callback",
+    dataType: "json"
+  }).done(function main(data) {
+    console.log(data);
+  });
 
-//TODO implement the jQuery ajax call here and console log the data
+//implement the jQuery ajax call here and console log the data
 
 //See https://api.jquery.com/jquery.get/ for an example of the get function
 
--- server.js
+++ server.js
@@ -5,7 +5,29 @@ var http = require('http');
 
 
 //TODO create a server using http
+function handleRequest(req, res){
+  if (req.url === '/' || req.url === '/index.html'){
+    // send index.html
+    fetchFile(req, res, './index.html');
+  } else if (req.url === '/app.js') {
+    // send app.js
+    fetchFile(req, res, './app.js');
+  } else if (req.url === '/api'){
+    // send data.json
+    fetchFile(req, res, './data.json');
+  } else {
+    // 404 not found
+    res.write('404 not found');
+    res.end();
+  }
+}
+
+var server = http.createServer(handleRequest);
+
 //TODO start the server on port 8000
+server.listen(8000, function(){
+  console.log("I'm listening on port 8000...");
+});
 
 
 function fetchFile(req, res, filename){
@@ -20,11 +42,12 @@ function handleFileLoad(req, res, err, data){
   if(err){ //if an error exists
    throw err;
   } else { // else we successfully loaded the file
-
-    //TODO set the status code to 200 to show that we succeeded.
-
-    //TODO write the file data to the response. We use toString to make it human readable.
-
-    //TODO send the response back to the client.
+    //set the status code to 200 to show that we succeeded.
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    //write the file data to the response. We use toString to make it human readable.
+    res.write(data.toString());
+    //send the response back to the client.
+    res.end();
   }
 }
